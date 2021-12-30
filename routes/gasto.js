136ec820ddb6e2e7fca7b6109123ea99d7d94fe2
federicodonner <federico.donner@gmail.com>
2021-12-30 00:21:23 -0300

    feat(gasto): implement get gasto from a specific month

--- routes/gasto.js
+++ routes/gasto.js
@@ -5,7 +5,7 @@ var db = require("../config/db.js");
 const { check, escape, validationResult } = require("express-validator");
 var messages = require("../data/messages.js");
 
-// Returns casa by hash
+// Create a new gasto
 router.post(
   "/:casaHash",
   [
@@ -102,6 +102,104 @@ router.post(
               res.status(201).send(results[0]);
             });
           });
+        });
+      });
+    });
+  }
+);
+
+// Get the gastos for the hashed casa in the especified month
+router.get(
+  "/:casaHash/mensual/:gastosAno/:gastosMes",
+  [
+    check("casaHash").escape(),
+    check("gastosAno").isNumeric().isFloat({ min: 2000 }),
+    check("gastosMes").isNumeric().isFloat({ max: 12, min: 1 }),
+  ],
+  (req, res) => {
+    // Validates that the parameters are correct
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // If one of them isn't, returns an error
+      return res.status(400).send(messages.PARAMETERS_ERROR);
+    }
+
+    // Loads the data into variables to use
+    var casaHash = req.params.casaHash;
+    var gastosAno = req.params.gastosAno;
+    var gastosMes = req.params.gastosMes;
+
+    // Verifies that the specified casa exists
+    let sql = "SELECT id FROM casa WHERE hash = '" + casaHash + "'";
+    let query = db.query(sql, (err, results) => {
+      if (err) {
+        throw err;
+      }
+      // Verify that there is at least one casa with that hash
+      if (!results.length) {
+        return res.status(404).send(messages.CASA_NOT_FOUND);
+      }
+
+      var casaId = results[0].id;
+
+      // Set the dates for the query
+      var startDate = new Date(gastosAno, gastosMes - 1);
+      startDate = Math.round(startDate / 1000);
+      var endDate = new Date(gastosAno, gastosMes);
+      endDate = Math.round(endDate / 1000 - 1);
+
+      // Get the categoria to fetch each gasto
+      sql =
+        "SELECT * FROM categoria WHERE casaId = " + casaId + " ORDER BY nombre";
+      query = db.query(sql, (err, categoriaResults) => {
+        if (err) {
+          throw err;
+        }
+
+        // Set up a promieses array to resolve when the
+        // gastos for each categoria return
+        var categoriaPromises = [];
+        categoriaResults.forEach((categoria, index) => {
+          // Get the gastos of the categoria
+          // Create a promise for each one and resolve when the query finishes
+          categoriaPromises.push(
+            new Promise((resolve, reject) => {
+              var categoriaId = categoria.id;
+              // Filter by the specified dates
+              sql =
+                "SELECT * FROM gasto WHERE categoriaId = " +
+                categoriaId +
+                " AND fecha >" +
+                startDate +
+                " AND fecha <" +
+                endDate +
+                " ORDER BY fecha";
+              query = db.query(sql, (err, gastosResults) => {
+                if (err) {
+                  throw err;
+                }
+                // When the query finishes, resolve the promise
+                resolve({ index: index, gastos: gastosResults });
+              });
+            })
+          );
+        });
+
+        // When all the promises resolve, populate the categorias
+        // and return the request
+        Promise.all(categoriaPromises).then((values) => {
+          values.forEach((value) => {
+            categoriaResults[value.index].gastos = value.gastos;
+          });
+
+          // Go over the categoria results and delete all
+          // that have no gastos for the period
+          categoriaResults.forEach((categoria, index) => {
+            if (!categoria.gastos.length) {
+              categoriaResults.splice(index, 1);
+            }
+          });
+          res.status(200).send(categoriaResults);
         });
       });
     });
